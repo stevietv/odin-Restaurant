e082b23bc9be61f940e2bb4afb8204fd66a5173c
Stevie Robinson <stevie.robinson@gmail.com>
2025-03-30 16:12:28 +0200

    add logo to header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { AddNavigation } from "./navigation";
+import { AddNavigation } from './navigation';
+import { Element } from './helpers/helpers'
 import './styles.css';
+import { Logo } from './images/images';
+
+let logoContainer = document.getElementsByClassName('logoContainer');
+
+let logoElement = Element('img', ['logo']);
+logoElement.src = Logo;
+logoContainer[0].appendChild(logoElement);
 
 AddNavigation();
